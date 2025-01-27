63343a45e26e00e9d5582ab4179f7d32c9d6d0aa
Alfredo Palacios <joseaph.1998@gmail.com>
2025-01-27 17:11:25 -0600

    feat: axios request interceptor added

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import {useAuthStore} from "@/store/auth_store.js";
+import {AuthService} from "@/services/auth_service.js";
 
 const apiClient = axios.create({
     baseURL: import.meta.env.VITE_APP_API_URL,
@@ -11,9 +13,38 @@ const apiClient = axios.create({
 apiClient.interceptors.response.use(
     response => response,
     error => {
-        console.error('Error en la solicitud:', error);
         return Promise.reject(error);
     }
 );
+
+apiClient.interceptors.request.use(
+    async config => {
+        const authStore = useAuthStore()
+        const authToken = AuthService.getToken()
+
+        if (authToken) {
+            config.headers.Authorization = `Bearer ${authToken}`;
+
+            const isTokenExpiring = checkTokenExpiry(authToken);
+            if (isTokenExpiring) {
+                try {
+                    await authStore.logOut()
+                } catch (error) {
+                    throw error;
+                }
+            }
+        }
+
+        return config;
+    },
+    error => Promise.reject(error)
+)
+
+const checkTokenExpiry = (token) => {
+    const [, payload] = token.split('.');
+    const decoded = JSON.parse(atob(payload));
+    const currentTime = Math.floor(Date.now() / 1000);
+    return decoded.exp - currentTime < 60;
+}
 
 export default apiClient;
